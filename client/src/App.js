64c2bbbd32350017b10e7c4a170718156adf3b2c
David Levens <52017470+Davidlevens@users.noreply.github.com>
2019-12-24 21:39:03 -0500

    Trying to get score and top score to work correctly

--- client/src/App.js
+++ client/src/App.js
@@ -31,33 +31,41 @@ class App extends Component {
     score: 0
   };
 
- clickedTiles = [
+  clickedTiles = [
 
- ];
-  
+  ];
+
 
   handleTileClick = (evt) => {
 
     const clickedTile = evt.target.src;
 
     if (this.clickedTiles.includes(clickedTile)) {
-      this.setState({score: 0});
+      this.setState({ score: 0 });
       console.log('ERRRR: already clicked, score: ', this.state.score);
       return;
     }
 
-    this.setState({score: this.state.score + 1});
+    const newScore = this.state.score + 1;
+    const topScore = newScore > this.state.topScore ?
+      newScore :
+      this.state.topScore;
+
     this.clickedTiles.push(clickedTile);
     console.log('score: ', this.state.score)
 
     const shuffled = this.state.tiles.sort(() => 0.5 - Math.random());
-    this.setState({ tiles: shuffled });
+    this.setState({
+      tiles: shuffled,
+      score: newScore,
+      topScore: topScore
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Score score={this.state.score} topScore={null} />
+        <Score score={this.state.score} topScore={this.state.topScore} />
         {
           this.state.tiles.map((tile, idx) => <Thumbnail
             src={tile} key={idx} onClick={this.handleTileClick}

9c87892312d51b925c7e4cfe17657c33b27b9992
David Levens <52017470+Davidlevens@users.noreply.github.com>
2019-12-27 23:18:19 -0500

    Fixed CorrectIncorrect display in Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Header.css";
+import CorrectIncorrect from "../CorrectIncorrect";
+
 
 
 const Header = props => (
     <div className="header">
         <ul>
             <li><h2 className=" title animated swing">Clicky Game</h2></li>
-            {/* /<GameMessage score={props.score} topScore={props.topScore} /> */}
+            <CorrectIncorrect score={props.score} topScore={props.topScore} />
             <li id="score">Score: {props.score} | Top Score: {props.topScore}</li>
         </ul>
         
